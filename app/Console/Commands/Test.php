4211b510998de0dfc8fbc79b1fff9372544ed664
Subasa <subasa_vn@haldata.net>
2025-02-03 10:50:12 +0700

    update

--- app/Console/Commands/Test.php
+++ app/Console/Commands/Test.php
@@ -4,6 +4,7 @@ namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\DB;
 
 class Test extends Command
 {
@@ -24,35 +25,131 @@ class Test extends Command
     /**
      * Execute the console command.
      *
-     * @return int
+     * @return void
      */
     public function handle()
     {
-        $filePath = sprintf('%s/textdata.csv', 'csv-imports');
+        $this->generateTruscoImgMapping();
+        // $this->checkProductCategories();
+    }
 
-        if (! Storage::disk('s3')->exists($filePath)) {
-            throw new \Exception('File not found');
-        }
+    private function checkProductCategories()
+    {
+        $handle = fopen(storage_path('category_trusco_01.csv'), 'r');
+        $lineNumber = 0;
+        $categories = [];
+        $notFoundCategories = [];
 
-        $stream = Storage::disk('s3')->readStream($filePath);
-        $batchData = [];
-        $rowNumber = 0;
-
-        while (($row = fgetcsv($stream, null, ',')) !== false) {
-            if ($rowNumber == 0) {
-                $rowNumber++;
-
+        while (($line = fgetcsv($handle)) !== false) {
+            if ($lineNumber == 0) {
+                $lineNumber++;
                 continue;
             }
 
-            $batchData[] = $row;
-            $rowNumber++;
+            $categoryId = '';
 
-            if (count($batchData) == 100) {
-                dd($batchData[0]);
-                $batchData = [];
+            if (isset($line[0]) && !empty($line[0])) {
+                $categoryId .= $line[0];
+            }
+
+            if (isset($line[1]) && !empty($line[1])) {
+                $categoryId .= "_" . $line[1];
+            }
+
+            if (isset($line[2]) && !empty($line[2])) {
+                $categoryId .= "_" . $line[2];
+            }
+
+            if ($categoryId === "___") {
+                $categoryId = "";
+            }
+
+            $categoryId = trim($categoryId);
+
+            if (! empty($categoryId) && ! in_array($categoryId, $categories)) {
+                $categories[] = $categoryId;
             }
         }
-        fclose($stream);
+
+        foreach ($categories as $category) {
+            $this->info('Processing ' . $category);
+
+            $categoryExists = DB::connection('catalog')
+                ->table('category')
+                ->where('category_id', $category)
+                ->exists();
+
+            if (! $categoryExists) {
+                $this->error('Category ' . $category . ' does not exist');
+                $notFoundCategories[] = $category;
+            } else {
+                $this->info('Category ' . $category . ' exists');
+            }
+        }
+
+        dd($notFoundCategories);
+    }
+    public function generateTruscoImgMapping()
+    {
+        $images = Storage::allFiles('trusco_images');
+        $total = count($images);
+        $mapping = [];
+        $notFounds = [];
+        $output = fopen(storage_path('trusco_img_mapping.csv'), 'w');
+        fwrite($output, "\xEF\xBB\xBF");
+        fputcsv($output, ['品目コード', '写真名']);
+
+        foreach ($images as $index => $image) {
+            $this->info('Processing ' . $image . ' (' . ($index + 1) . '/' . $total . ')');
+            $originalFileName = basename($image);
+            $fileName = strtoupper($originalFileName);
+            $fileName = str_replace(['.jpg', '.png', '.jpeg', '.JPG', '.PNG', '.JPEG'], '', $fileName);
+            $formattedFileName = str_replace([' ', '_'], '', $fileName);
+            $products = DB::connection('catalog')
+                ->table('oc_product')
+                ->select('sku')
+                ->whereRaw('REPLACE(sku, " ", "") = ?', [$formattedFileName])
+                ->get();
+
+            $this->info('Found ' . $products->count() . ' products for ' . $fileName . ' with formatted name ' . $formattedFileName);
+
+            if ($products->count() > 0) {
+                if ($products->count() > 1) {
+                    info('Multiple products found for ' . $fileName, [
+                        'fileName' => $fileName,
+                        'products' => $products->pluck('sku')->toArray(),
+                        'formattedFileName' => $formattedFileName,
+                    ]);
+                    $this->info('Multiple products found for ' . $fileName);
+                } else {
+                    $sku = $products->first() ? $products->first()->sku : null;
+
+                    if (!empty($sku)) {
+                        $this->info('Product found for ' . $fileName . ' with code ' . $sku);
+                        $mapping[] = [
+                            'sku' => $products->first()->sku,
+                            'fileName' => $fileName,
+                        ];
+                        fputcsv($output, [$sku, $originalFileName]);
+                    }
+
+                    Storage::delete($image);
+                    $this->info('Deleted image: ' . $image);
+                }
+            } else {
+                $this->warn('No product found for ' . $fileName);
+                $notFounds[] = $fileName;
+            }
+
+            $this->info('Done ' . $image);
+            $this->info('--------------------------------');
+        }
+
+        if (!empty($notFounds)) {
+            info('Not founds images', ['notFound' => $notFounds]);
+        }
+
+        fclose($output);
+        $this->info('Done!');
     }
 }
--- config/database.php
+++ config/database.php
@@ -63,6 +63,26 @@ return [
             ]) : [],
         ],
 
+        'catalog' => [
+            'driver' => 'mysql',
+            'host' => env('DB_CATALOG_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '3306'),
+            'database' => env('DB_CATALOG_DATABASE', 'forge'),
+            'username' => env('DB_CATALOG_USERNAME', 'forge'),
+            'password' => env('DB_CATALOG_PASSWORD', ''),
+            'unix_socket' => env('DB_SOCKET', ''),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'strict' => true,
+            'engine' => null,
+            'options' => extension_loaded('pdo_mysql') ? array_filter([
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+                PDO::MYSQL_ATTR_LOCAL_INFILE => true,
+            ]) : [],
+        ],
+
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
@@ -125,7 +145,7 @@ return [
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
-            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_database_'),
+            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_') . '_database_'),
         ],
 
         'default' => [
